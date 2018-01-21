bdb08c4b08bc6fd6d432ea90b6a3b58b2c92fce9
Paul Sheets <paulsheets26@gmail.com>
2018-01-20 21:52:55 -0700

    Added gem pry

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,11 +25,6 @@ module SessionsHelper
     end
   end
 
-  # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   def logged_in?
     !current_user.nil?
   end
